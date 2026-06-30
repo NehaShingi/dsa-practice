165d252a85645f8283e67b0ca0071da3b54121ef
NehaShingi <shingineha836@gmail.com>
2026-06-30 12:40:54 +0530

    Solved Upper Bound using binary search

---
+++ BinarySearch/UpperBound.cpp
@@ -0,0 +1,47 @@
+/*
+Given a sorted array of N integers and an integer x, write a program to find the upper bound of x
+
+The upper bound is the smallest index, ind, where arr[ind] > x. But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array.
+
+Time Complexity: O(log N)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findUpperBound(vector<int> &arr, int key)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = arr.size();
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > key)
+        {
+            ans = mid;      // store possible answer
+            high = mid - 1; // serach in left half for smaller index
+        }
+        else
+        {
+            // arr[mid]<=key, so search in right half
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 2, 3};
+    int key = 2;
+    int upperBound = findUpperBound(arr, key);
+
+    cout << "Upper Bound:" << upperBound;
+
+    return 0;
+}
