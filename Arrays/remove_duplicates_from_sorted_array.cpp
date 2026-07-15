f09034cb6ce20e938e227a215c5b1bb2354f13d7
NehaShingi <shingineha836@gmail.com>
2026-07-15 06:03:05 +0530

    Optimize solution for LeetCode 26 using two pointers

--- Arrays/remove_duplicates_from_sorted_array.cpp
+++ Arrays/remove_duplicates_from_sorted_array.cpp
@@ -1,6 +1,10 @@
 /*
-    Time Complexity: O(n)
-    Space Complexity: O(n)
+Leetcode Problem - 26
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
 
 #include <iostream>
@@ -9,19 +13,16 @@ using namespace std;
 
 void remove_duplicates(vector<int> &arr)
 {
-    int i = 0, j = 0;
+    int i = 0, j = 1;
     while (j < arr.size())
     {
-        if (arr[i] == arr[j])
+        while (arr[i] == arr[j])
         {
             j++;
         }
-        else
-        {
-            i++;
-            swap(arr[i], arr[j]);
-            j++;
-        }
+        i++;
+        swap(arr[i], arr[j]);
+        j++;
     }
 
     for (int k = 0; k <= i; k++)
